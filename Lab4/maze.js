622cce9a3154e76aeefc5afc6813a28ce4fdb701
Karlculations <karlgoddard0013@gmail.com>
2016-10-11 22:49:48 -0500

    All boundaries glow red on hover

--- Lab4/maze.js
+++ Lab4/maze.js
@@ -1,10 +1,18 @@
 window.onload= function(){
 	
 	var boundary1 = document.getElementById("boundary1")
+	var boundary = document.querySelectorAll(".boundary"); //returns an array
 	
 	boundary1.addEventListener("mouseover", mouseOver);
 	
+	for (var j = 0; j < boundary.length-1; j++){
+		boundary[j].addEventListener("mouseover", mouseOver);
+	}
+	
 	function mouseOver() {
 		boundary1.className = "boundary youlose";
+		for (var i = 0; i<boundary.length-1; i++){ //stop one short because we don't want to change the example border under the maze
+			boundary[i].className += " youlose";
+		};
 	};
 };
