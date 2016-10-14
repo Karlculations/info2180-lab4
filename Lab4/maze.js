b58b7c105f616fa086d7e496a78aec5f6fdd1f57
Karlculations <karlgoddard0013@gmail.com>
2016-10-14 18:44:27 -0500

    Disallow cheating

--- Lab4/maze.js
+++ Lab4/maze.js
@@ -1,20 +1,20 @@
 window.onload = function(){ //load elements when the page/window loads
 	
-	
 	var start = document.getElementById("start");
 	var end = document.getElementById("end");
 	var boundary1 = document.getElementById("boundary1");
 	var boundary = document.querySelectorAll(".boundary"); //returns an array
-	var hit = false;
+	var hit = false; //used to keep track of whether the player hit a wall(boundary) or not
+	var maze = document.getElementById("maze");
+		
+	start.addEventListener("mouseover", beginMaze); //being the maze when mouse is over S
 	
-	start.addEventListener("mouseover", beginMaze);
-	
-	function restart(){
+	function restart(){ //reload the page by clicking on S
 		location.reload();
 	};
 	
-	function endMaze(){
-			if (hit === true){
+	function endMaze(){ 
+			if (hit){ //Is hit true? Did the player hit a wall?
 				//alert("You lose!");
 				document.getElementById("status").innerHTML = "You lose!";
 			}else{
@@ -23,7 +23,19 @@ window.onload = function(){ //load elements when the page/window loads
 			}
 	};
 	
+	function mouseOver(){ //event handler for when the mouse is over the boundaries
+		boundary1.className = "boundary youlose"; //change boundary1 classes
+		for (var i = 0; i<boundary.length-1; i++){ //stop one short because we don't want to change the example border under the maze
+			boundary[i].className = "boundary youlose"; //attach this class name to the current class name
+		}
+		hit = true; //player hits a wall (boundary)
+	};
+		
 	function beginMaze(){
+			maze.onmouseleave = function(){
+			mouseOver();
+			endMaze();
+		}
 		start.addEventListener("click", restart);
 		boundary1.addEventListener("mouseover", mouseOver);
 		
@@ -31,15 +43,7 @@ window.onload = function(){ //load elements when the page/window loads
 			boundary[j].addEventListener("mouseover", mouseOver);
 		}
 		
-		function mouseOver(){
-			boundary1.className = "boundary youlose";
-			for (var i = 0; i<boundary.length-1; i++){ //stop one short because we don't want to change the example border under the maze
-				boundary[i].className += " youlose";
-			}
-			hit = true;
-		};		
-		
-		end.addEventListener("mouseover", endMaze);	
+		end.addEventListener("mouseover", endMaze);	//end the maze when the mouse is over E
 	};
 	
 };
