6712705138b7bb2b3432b225747f5beaf97c59ec
Karlculations <karlgoddard0013@gmail.com>
2016-10-14 00:17:23 -0500

    Alerts on successful completion of maze

--- Lab4/maze.js
+++ Lab4/maze.js
@@ -1,18 +1,38 @@
-window.onload= function(){
+window.onload = function(){ //load elements when the page/window loads
 	
-	var boundary1 = document.getElementById("boundary1")
+	
+	var start = document.getElementById("start");
+	var end = document.getElementById("end");
+	var boundary1 = document.getElementById("boundary1");
 	var boundary = document.querySelectorAll(".boundary"); //returns an array
+	var hit = false;
 	
-	boundary1.addEventListener("mouseover", mouseOver);
+	start.addEventListener("mouseover", beginMaze);
 	
-	for (var j = 0; j < boundary.length-1; j++){
-		boundary[j].addEventListener("mouseover", mouseOver);
-	}
-	
-	function mouseOver() {
-		boundary1.className = "boundary youlose";
-		for (var i = 0; i<boundary.length-1; i++){ //stop one short because we don't want to change the example border under the maze
-			boundary[i].className += " youlose";
+	function beginMaze(){
+		boundary1.addEventListener("mouseover", mouseOver);
+		
+		for (var j = 0; j < boundary.length-1; j++){
+			boundary[j].addEventListener("mouseover", mouseOver);
+		}
+		
+		function mouseOver(){
+			boundary1.className = "boundary youlose";
+			for (var i = 0; i<boundary.length-1; i++){ //stop one short because we don't want to change the example border under the maze
+				boundary[i].className += " youlose";
+			}
+			hit = true;
+		};		
+		
+		end.addEventListener("mouseover", endMaze);
+		
+		function endMaze(){
+			if (hit === true){
+				alert("You lose");
+			}else{
+				alert("You win!");
+			}
 		};
 	};
+	
 };
